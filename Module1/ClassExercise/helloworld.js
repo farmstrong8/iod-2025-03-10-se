1a82dff2814e941a382bfef7247ff19c20cd15a1
Ferris Armstrong <fda@bu.edu>
2025-03-17 20:03:33 -0400

    simple project and gitignore

--- Module1/ClassExercise/helloworld.js
+++ Module1/ClassExercise/helloworld.js
@@ -1,1 +1,25 @@
 console.log("hello world, 'this is in quotes'");
+
+function sayHello() {
+    console.log("Hello this my hello function");
+
+    console.log("Hello this my hello function");
+    console.log("Hello this my hello function");
+    console.log("Hello this my hello function");
+    console.log("Hello this my hello function");
+}
+
+// this is supposed to call the function
+sayHello();
+
+function sum(a, b) {
+    console.log(a + b);
+
+    console.log("THIS IS BEFORE THE RETURN");
+
+    return a + b;
+
+    console.log("THIS IS AFTER THE RETURN");
+}
+
+sum(5, 1);
