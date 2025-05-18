40c25ee1eb09c95147b874df1c7c6d6d4cc86642
Ferris Armstrong <fda@bu.edu>
2025-05-17 20:11:07 -0400

    2025.5.17

--- Module5/express/index.js
+++ Module5/express/index.js
@@ -4,10 +4,15 @@ const port = 3000;
 const petRoutes = require("./routes/pet.routes");
 const calculatorRoutes = require("./routes/calculator.routes");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 app.use("/", express.static("public"));
 
 app.use("/pet-api", petRoutes);
 app.use("/calculator", calculatorRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
