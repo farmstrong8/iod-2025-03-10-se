4582aa080851d388939a7a14abb8b0286ffecc85
Ferris Armstrong <fda@bu.edu>
2025-07-07 19:50:45 -0400

    7.7.2025

--- Module8/mvc-mongo/server.js
+++ Module8/mvc-mongo/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const dbConnect = require("./dbConnect");
+const userRoutes = require("./routes/userRoutes");
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -9,6 +10,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to my MongoDB application." });
 });
+
+app.use("/api/users", userRoutes);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
